9a8ba4ab4dff02b83be19d60bb2764a0198e0384
jianfang.chen <jianfang.chen@pixocial.com>
2023-01-17 13:26:46 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import { Row, Col, Input, Button, message, Space } from 'antd';
+import extract from './utils/extract';
+
+const { TextArea } = Input;
+
+const buttonStyle = {display: 'flex', justifyContent: 'center', alignItems: 'center'}
 
 function App() {
+  const [str, setStr] = useState('');
+  const [extractStr, setExtractStr] = useState('');
+
+  const extractClick = async () => {
+    if (!str) {
+      return message.info('请输入内容')
+    }
+    let data =  extract(str);
+
+    if (Array.isArray(data)) {
+      let resultList = []; // 去重结果
+      let repeatList = []; // 重复结果
+
+      // 判断重复项
+      data.forEach((item) => {
+        if (resultList.includes(item)) {
+          // 重复
+          repeatList.push(item);
+        } else {
+          resultList.push(item);
+        }
+      })
+
+      resultList = resultList.join('\n');
+      repeatList = repeatList.join('\n');
+
+      data = `${resultList}`;
+      if (repeatList.length) {
+        data += `\n\n\n重复项：\n${repeatList}`
+      }
+
+      message.success('提取成功!');
+    }
+
+    setExtractStr(data);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{margin: '20px'}}>
+      <Row justify="space-around" align="middle">
+        <Col span={15}>
+          <TextArea rows={30} value={str} onChange={(e) => setStr(e.target.value)} />
+        </Col>
+        <Col span={2} style={buttonStyle}>
+          <Space size="small" direction="vertical">
+            <Button type="primary" onClick={extractClick}>提取</Button>
+          </Space>
+        </Col>
+        <Col span={7}>
+          <TextArea rows={30} value={extractStr} onChange={(e) => setExtractStr(e.target.value)}  />
+        </Col>
+      </Row>
     </div>
   );
 }
